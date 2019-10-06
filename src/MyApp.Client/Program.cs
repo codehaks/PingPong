82fca941c57e66e2c89b26e7f3137ec02c2a8ef6
Codehaks <ghods.hakim@gmail.com>
2019-10-07 03:21:09 +0330

    Draft

--- src/MyApp.Client/Program.cs
+++ src/MyApp.Client/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
 
 namespace MyApp.Client
 {
@@ -6,7 +9,36 @@ namespace MyApp.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                TcpClient Tcpclient = new TcpClient();
+
+                Console.WriteLine("Connecting..");
+                Tcpclient.Connect("127.100.100.50", 3000);
+                Console.WriteLine("Connected");
+                Console.WriteLine("Ente the String you want to send ");
+
+                string str = Console.ReadLine();
+                Stream stm = Tcpclient.GetStream();
+
+                ASCIIEncoding ascnd = new ASCIIEncoding();
+                byte[] ba = ascnd.GetBytes(str);
+                //Console.WriteLine("Sending..");
+                stm.Write(ba, 0, ba.Length);
+                byte[] bb = new byte[100];
+                int k = stm.Read(bb, 0, 100);
+                for (int i = 0; i < k; i++)
+                {
+                    Console.Write(Convert.ToChar(bb[i]));
+                }
+
+                Tcpclient.Close();
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error" + ex.StackTrace);
+            }
         }
     }
 }
--- src/MyApp.Server/Program.cs
+++ src/MyApp.Server/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace MyApp.Server
 {
@@ -6,7 +9,39 @@ namespace MyApp.Server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var ip = IPAddress.Parse("127.100.100.50");
+            TcpListener connection = new TcpListener(ip, 3000);
+
+            connection.Start();
+
+            Console.WriteLine($"Server is Running [{connection.LocalEndpoint}]");
+            
+            Console.WriteLine("Waiting for Connections...");
+
+            Socket s = connection.AcceptSocket();
+
+            Console.WriteLine("Connection Accepted From:" + s.RemoteEndPoint);
+
+            byte[] b = new byte[100];
+            int k = s.Receive(b);
+
+            Console.WriteLine("Recieved..");
+
+            for (int i = 0; i < k; i++)
+            {
+                Console.Write(Convert.ToChar(b[i]));
+            }
+
+            var encode = new ASCIIEncoding();
+
+            s.Send(encode.GetBytes("Pong"));
+
+            Console.WriteLine("\n Pong Sent");
+
+            s.Close();
+            connection.Stop();
+
+            Console.ReadLine();
         }
     }
 }

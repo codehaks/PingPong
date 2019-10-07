05216310763aefa7429396e6807e0431161db35e
Codehaks <ghods.hakim@gmail.com>
2019-10-07 05:59:40 +0330

    clean-up

--- src/MyApp.Server/Program.cs
+++ src/MyApp.Server/Program.cs
@@ -10,37 +10,7 @@ namespace MyApp.Server
     {
         static async Task Main(string[] args)
         {
-            //var ip = IPAddress.Parse("127.100.100.50");
-            //TcpListener connection = new TcpListener(ip, 3000);
-
-            //connection.Start();
-
-            //Console.WriteLine($"Server is Running [{connection.LocalEndpoint}]");
-
-            //Console.WriteLine("Waiting for Connections...");
-
-            //Socket s = connection.AcceptSocket();
-
-            //Console.WriteLine("Connection Accepted From:" + s.RemoteEndPoint);
-
-            //byte[] b = new byte[100];
-            //int k = s.Receive(b);
-
-            //Console.WriteLine("Recieved..");
-
-            //for (int i = 0; i < k; i++)
-            //{
-            //    Console.Write(Convert.ToChar(b[i]));
-            //}
-
-            //var encode = new ASCIIEncoding();
-
-            //s.Send(encode.GetBytes("Pong"));
-
-            //Console.WriteLine("\n Pong Sent");
-
-            //s.Close();
-            //connection.Stop();
+         
 
             var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             var address = IPAddress.Parse("127.100.100.50");

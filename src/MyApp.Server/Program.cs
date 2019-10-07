64c7932e6d82a09dd4cb11727c9e26cd4284b67a
Codehaks <ghods.hakim@gmail.com>
2019-10-07 15:58:54 +0330

    Server

--- src/MyApp.Server/Program.cs
+++ src/MyApp.Server/Program.cs
@@ -10,47 +10,50 @@ namespace MyApp.Server
     {
         static async Task Main(string[] args)
         {
-         
+
+            var server = new Vega.Server("127.100.100.50", 3000);
+
+            await server.Start();
 
             // dispose
-            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-            var address = IPAddress.Parse("127.100.100.50");
-            var port = 3000;
+            //var address = IPAddress.Parse("127.100.100.50");
+            //var port = 3000;
 
-            var endpoint = new IPEndPoint(address, port);
+            //var endpoint = new IPEndPoint(address, port);
 
-            socket.Bind(endpoint);
-            socket.Listen(10);
-            Console.WriteLine("Starting...");
-            while (true)
-            {
-                Console.WriteLine("Waiting connection ... ");
+            //socket.Bind(endpoint);
+            //socket.Listen(10);
+            //Console.WriteLine("Starting...");
+            //while (true)
+            //{
+            //    Console.WriteLine("Waiting connection ... ");
 
-                Socket serverSocket = socket.Accept();
+            //    Socket serverSocket = socket.Accept();
 
-                byte[] bytes = new Byte[1024];
-                string data = null;
+            //    byte[] bytes = new Byte[1024];
+            //    string data = null;
 
-                while (true)
-                {
-                    Console.WriteLine("\n Ping received. ");
-                    int numByte = serverSocket.Receive(bytes);
+            //    while (true)
+            //    {
+            //        Console.WriteLine("\n Ping received. ");
+            //        int numByte = serverSocket.Receive(bytes);
 
-                    data += Encoding.ASCII.GetString(bytes, 0, numByte);
+            //        data += Encoding.ASCII.GetString(bytes, 0, numByte);
 
-                    break; // assuming message size is less than 1KB
-                }
+            //        break; // assuming message size is less than 1KB
+            //    }
 
-                Console.WriteLine("Request -> {0} ", data);
-                byte[] message = Encoding.ASCII.GetBytes("Pong");
-                await serverSocket.SendAsync(message, SocketFlags.None);
-                Console.WriteLine("Response-> {0} ", message);
+            //    Console.WriteLine("Request -> {0} ", data);
+            //    byte[] message = Encoding.ASCII.GetBytes("Pong");
+            //    await serverSocket.SendAsync(message, SocketFlags.None);
+            //    Console.WriteLine("Response-> {0} ", message);
 
-                serverSocket.Shutdown(SocketShutdown.Both);
-                serverSocket.Close();
-                break;
-            }
+            //    serverSocket.Shutdown(SocketShutdown.Both);
+            //    serverSocket.Close();
+            //    break;
+            //}
 
 
         }
--- src/Vega/Server.cs
+++ src/Vega/Server.cs
@@ -1,10 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace Vega.Server
+namespace Vega
 {
-    class Server
+    public class Server
     {
+        private readonly string _ip;
+        private readonly int _port;
+
+        private Socket socket;
+        private Socket serverSocket;
+
+        public Server(string ip, int port)
+        {
+            _ip = ip;
+            _port = port;
+        }
+
+        public async Task Start()
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var address = IPAddress.Parse(_ip);
+            var endpoint = new IPEndPoint(address, _port);
+
+            socket.Bind(endpoint);
+            socket.Listen(10);
+
+            while (true)
+            {
+                Console.WriteLine("Waiting connection ... ");
+
+                serverSocket = socket.Accept();
+
+                byte[] requestBytes = new Byte[1024];
+                string requestMessage = null;
+
+                while (true)
+                {
+                    
+                    int requestLength = serverSocket.Receive(requestBytes);
+
+                    requestMessage += Encoding.ASCII.GetString(requestBytes, 0, requestLength);
+                    Console.WriteLine($"\n {requestMessage} received. ");
+
+                    break; // assuming message size is less than 1KB
+                }
+
+                byte[] message = Encoding.ASCII.GetBytes("Pong");
+                await serverSocket.SendAsync(message, SocketFlags.None);
+
+            }
+        }
+
+        public void Shutdown()
+        {
+            serverSocket.Shutdown(SocketShutdown.Both);
+            serverSocket.Close();
+        }
     }
 }
